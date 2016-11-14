4423d5bad7c4b7bb182b9767359d47aa227118be
vinny366 <vvutukur@asu.edu>
2016-11-14 13:44:08 -0700

    parkingApp

---
+++ parkingApplication/server.js
@@ -0,0 +1,32 @@
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+// database connection
+mongoose.connect(configDB.url); 
+require('./config/passport')(passport); // pass passport for configuration
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+app.use(session({ secret: 'vin_parking' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The server runs on port ' + port);
